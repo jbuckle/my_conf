b353d08f654c0848cf80a567de471c02c6f23651
Will Jackson <wjackson64@gmail.com>
2014-11-18 20:14:42 +0000

    Get solarized to work with tmux.

--- conf.sh
+++ conf.sh
@@ -1,2 +1,6 @@
 ln -s ~/my_conf/.tmux.conf ~/.tmux.conf 
 ln -s ~/my_conf/.vimrc ~/.vimrc 
+
+if [ -f ~/.zshrc ] && ! grep -q 'alias tmux' ~/.zshrc; then 
+  echo alias tmux=\"TERM=xterm-256color tmux\" >> ~/.zshrc
+fi
